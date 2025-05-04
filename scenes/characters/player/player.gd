b26e6154a5f5574ea0ba393ac84e2665e25ae98a
Dumitru Postoronca <postoroncadumitru@gmail.com>
2025-05-04 15:47:07 +0300

    feat: setted tools panel to select the current tool
    feat: added more collectables
    milk, corn, egg, tomato

--- scenes/characters/player/player.gd
+++ scenes/characters/player/player.gd
@@ -1,5 +1,15 @@
 class_name Player
 extends CharacterBody2D
 
+@onready var hit_component: HitComponent = $HitComponent
+
 @export var currentTool: DataTypes.Tools = DataTypes.Tools.None
 var player_direction: Vector2
+
+func _ready() -> void:
+	ToolManager.tool_selected.connect(on_tool_selected)
+	
+func on_tool_selected(tool: DataTypes.Tools)->void:
+	currentTool = tool
+	hit_component.current_tool = tool
+	print("Tool:", tool)
