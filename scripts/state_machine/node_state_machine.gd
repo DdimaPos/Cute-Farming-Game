8fbec73261217e8950f064f64bfff70c874f1e06
Dumitru Postoronca <postoroncadumitru@gmail.com>
2025-05-04 12:32:13 +0300

    feat: have setup navigation agents for npcs

--- scripts/state_machine/node_state_machine.gd
+++ scripts/state_machine/node_state_machine.gd
@@ -21,6 +21,7 @@ func _ready() -> void:
 		initial_node_state._on_enter()
 		current_node_state = initial_node_state
 		current_node_state_name = current_node_state.name.to_lower()
+		current_node_state_name = current_node_state.name.to_lower()
 
 
 func _process(delta : float) -> void:
@@ -32,7 +33,7 @@ func _physics_process(delta: float) -> void:
 	if current_node_state:
 		current_node_state._on_physics_process(delta)
 		current_node_state._on_next_transitions()
-		#print(parent_node_name, " Current State: ", current_node_state_name)
+		print(parent_node_name, " Current State: ", current_node_state_name)
 
 
 func transition_to(node_state_name : String) -> void:
@@ -51,4 +52,4 @@ func transition_to(node_state_name : String) -> void:
 	
 	current_node_state = new_node_state
 	current_node_state_name = current_node_state.name.to_lower()
-	#print("Current State: ", current_node_state_name)
+	print("Current State: ", current_node_state_name)
